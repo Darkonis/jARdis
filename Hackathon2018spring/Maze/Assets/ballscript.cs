b4530231e8a167ce0a4dc1df4d5a738ba3554011
Darkonis <flameshadow98@yahoo.com>
2018-03-25 06:52:21 -0500

    Updates from Spring 2018 Hackathon
    THe entire code files for the apk of the maze and first app

---
+++ Hackathon2018spring/Maze/Assets/ballscript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballscript : MonoBehaviour {
+    public GameObject plane;
+    public GameObject spawnPoint;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(plane.transform.position.y-20>transform.position.y)
+        {
+            
+            transform.position = spawnPoint.transform.position;
+        }
+	}
+}
